95168133c6155701af01860e3e5a842a11b34141
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-29 22:54:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,4 +18,7 @@ def remove_strawberry(contacts)
     contacts.each do |person , data|
       
       data.each do |attribute , data|
+        
+        if attribute == favorite_ice_cream_flavors: 
+        favorite_ice_cream_flavors = favorite_ice_cream_flavors.shift
 end
